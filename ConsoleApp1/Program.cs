30a2b81da7abaf573bfb42a4a7a4f25e5ba2609b
cesaaargm <75700899+cesaaargm@users.noreply.github.com>
2022-08-05 22:48:33 +0200

    ploaded the files directy
    fsfsadfdsfdfdsafdsafdsfdsfdsaf

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConsoleApp1.characters.warriors;
+
+public class EntryType
+{
+    static void Main()
+    {
+        System.Console.WriteLine("Hello World!");
+        Warrior goodWarrior = new Warrior(180, 90,"JOhnny");
+        Warrior badWarrior = new Warrior(190, 80,"McFly");
+
+        
+        //Console.WriteLine(goodWarrior.Height);
+        //Console.WriteLine(goodWarrior.Weigth);
+
+        goodWarrior.Greetings(badWarrior);  
+        badWarrior.Greetings(goodWarrior);  
+
+    }
+}
+
+
---
+++ Warrior.cs
@@ -0,0 +1,24 @@
+﻿public class Warrior
+{
+
+    public Warrior(int height, int weight, string name)
+    {
+        Height = height;
+        Weight = weight;
+        Name = name;
+    }
+    private int height;
+    private int weight;
+    private string name;
+
+    public int Height { get => height; set => height = value; }
+    public int Weight { get => weight; set => weight = value; }
+    public string Name { get => name; set => name = value; }
+
+
+    public void Greetings(Warrior warrior) //le estoy pasando todo el objeto?
+    {
+        Console.WriteLine($@"Greetings, {warrior.Name}!");
+    }
+
+}
