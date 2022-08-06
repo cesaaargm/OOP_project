2e83e3dc2897d30d7e881bf21572234d493fab11
Ghionoiu Martinez <cghionoiu@luxoft.com>
2022-08-06 11:47:27 +0200

    Added properties to Assasin, TBD: name field and properties of other charcters

--- ConsoleApp1/characters/Melee/Assassin.cs
+++ ConsoleApp1/characters/Melee/Assassin.cs
@@ -6,32 +6,173 @@ namespace MagicMonsters.characters.Melee
 {
     class Assassin
     {
+        #region fields
+
         private int abilityPoints;
         private int healthPoints;
         private int level;
-        private int name;
 
+        private string name;
         private string faction;
 
         private LightLeatherVest bodyArmour;
         private Sword weapon;
 
+
+        #endregion fields
+
+        #region Properties
+
+        public int AbilityPoints 
+        { 
+            get => abilityPoints;
+
+            set
+            {
+                if (value >= 0)
+                {
+                    try
+                    {
+                        abilityPoints = value;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                     throw new ArgumentOutOfRangeException(string.Empty, "Ability points must be a positive number! Not assigned");
+                }
+            } 
+        }
+        public int HealthPoints
+        {
+            get => healthPoints;
+
+            set
+            {
+                if (value >= 0)
+                {
+                    try
+                    {
+                        healthPoints = value;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Health points must be a positive number! Not assigned");
+                }
+            }
+        }
+        public int Level
+        {
+            get => level;
+
+            set
+            {
+                if (value > 0 && value <= 100)
+                {
+                    try
+                    {
+                        level = value;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "Level must be between 1 and 100");
+                }
+            }
+        }
+       
+        public string Name
+        {
+            get => name;
+
+            set
+            {
+                if (0==0)
+                {
+                    try
+                    {
+                        name = value;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "TBd");
+                }
+            }
+        }
+        public string Faction
+        {
+            get => faction;
+
+            set
+            {
+                if (0 == 0)
+                {
+                    try
+                    {
+                        faction = value;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, "TBd");
+                }
+            }
+        }
+
+        internal LightLeatherVest BodyArmour 
+        {
+            get => bodyArmour;
+
+            set => bodyArmour = value; 
+        }
+        internal Sword Weapon 
+        {
+            get => weapon; 
+            set => weapon = value;
+        }
+
+
+        #endregion Properties
+
+        #region Constructors
         public Assassin()
         {
             throw new NotImplementedException();
         }
 
-        public Assassin(int abilityPoints, string faction, int healthPoints, int level, int name, LightLeatherVest bodyArmour, Sword weapon)
+        public Assassin(int abilityPoints, string faction, int healthPoints, int level, string name, LightLeatherVest bodyArmour, Sword weapon)
         {
-            this.abilityPoints = abilityPoints;
-            this.faction = faction;
-            this.healthPoints = healthPoints;
-            this.level = level;
-            this.name = name;
-            this.bodyArmour = bodyArmour;
-            this.weapon = weapon;
+            this.AbilityPoints = abilityPoints;
+            this.Faction = faction;
+            this.HealthPoints = healthPoints;
+            this.Level = level;
+            this.Name = name;
+            this.BodyArmour = bodyArmour;
+            this.Weapon = weapon;
         }
 
+        #endregion Constructors
         // Methods
 
         public void Raze()
